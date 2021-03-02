30d61158ff583cea21a205b5d68dd51f5df6cacb
Sergo58 <godfather59@yandex.ru>
2021-03-02 18:29:20 +0500

    second step refactor

--- src/contacts/Contacts.js
+++ src/contacts/Contacts.js
@@ -8,37 +8,42 @@ import {faMailchimp} from "@fortawesome/free-brands-svg-icons";
 import {faEnvelopeOpen, faPhoneSquare} from "@fortawesome/free-solid-svg-icons";
 
 
-
-
-
-
 export function Contacts() {
     return (
 
-            <div className={style.contactContainer}>
+        <div className={style.contactContainer}>
 
-                <div className={style.contact}>
-                    <Title first={"get in "} second={"touch"}/>
-
-                    <div className={style.input} >
-                        <div>
-                            <div ><div className={style.mail}><FontAwesomeIcon size={"3x"} icon={faEnvelopeOpen}/></div> <p className={style.iconTitle}>MAIL ME</p> <p className={style.iconContent}>Godfather59@yandex.ru</p> </div>
-                            <div className={style.iconMain}><div className={style.mail}><FontAwesomeIcon size={"3x"} icon={faPhoneSquare}/></div><p className={style.iconTitle}>CALL ME</p> <p className={style.iconContent}>+7 919 381 30 98</p></div>
-                        </div>
+            <div className={style.contact}>
+                <Title first={"get in "} second={"touch"}/>
+                <div className={style.container}>
 
 
-                        <form  action="">
-                            <input placeholder={"YOUR NAME"}  type="text"/><br/>
-                            <input placeholder={"YOUR EMAIL"}  type="text"/><br/>
-                            <textarea placeholder={"YOUR MESSAGE"} ></textarea>
+
+                    <div className={style.input}>
+                        <form action="">
+                            <input placeholder={"YOUR NAME"} type="text"/><br/>
+                            <input placeholder={"YOUR EMAIL"} type="text"/><br/>
+                            <textarea placeholder={"YOUR MESSAGE"}></textarea>
                         </form>
-                         </div>
-                    <div className={style.icon}>
-                        SEND MESSAGE
+                        <div className={style.icon}>
+                            SEND MESSAGE
+                        </div>
                     </div>
-
+                    <div className={style.item}>
+                        <div>
+                            <div className={style.mail}><FontAwesomeIcon size={"3x"} icon={faEnvelopeOpen}/></div>
+                            <p className={style.iconTitle}>MAIL ME</p> <p
+                            className={style.iconContent}>Godfather59@yandex.ru</p></div>
+                        <div className={style.iconMain}>
+                            <div className={style.mail}><FontAwesomeIcon size={"3x"} icon={faPhoneSquare}/></div>
+                            <p className={style.iconTitle}>CALL ME</p> <p className={style.iconContent}>+7 919 381 30
+                            98</p></div>
+                    </div>
                 </div>
+
+
             </div>
+        </div>
 
 
     );
